70e1aa73285f7e696d6e7e0b119edc8a5f73e83c
Rubai -Rahman <rubairahman1@gmail.com>
2025-02-20 13:02:50 +0600

    path fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'tailwindcss';
-import animate from 'tailwindcss-animate';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -59,5 +59,5 @@ export default {
       },
     },
   },
-  plugins: [animate],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
